df8eae99a80ebdab647f7e8abe86be3fa4c808c0
qualmless <terasmash@gmail.com>
2020-02-12 14:14:44 -0600

    added coding events folder

--- src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
@@ -1,4 +1,4 @@
-package org.launchcode.codingevents;
+`package org.launchcode.codingevents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
