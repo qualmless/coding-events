1d5816e05d5647b51a38f7312d788cd8f646430f
qualmless <terasmash@gmail.com>
2020-02-20 15:38:00 -0600

    12.3.2 complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
 
     private int id;
@@ -40,5 +42,19 @@ public class Event {
     public String toString() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 
 }
