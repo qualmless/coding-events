d682da99953b1c1903881ab6a9e999286a12b18c
qualmless <terasmash@gmail.com>
2020-02-20 13:30:59 -0600

    version control

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,16 +1,14 @@
 package org.launchcode.codingevents.controllers;
 
-<<<<<<< Updated upstream
-import org.launchcode.codingevents.data.EventData;
-=======
->>>>>>> Stashed changes
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -20,15 +18,12 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-<<<<<<< Updated upstream
-=======
     private static List<Event> events = new ArrayList<>();
 
->>>>>>> Stashed changes
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", events);
         return "events/index";
     }
 
@@ -39,45 +34,9 @@ public class EventController {
     }
 
     @PostMapping("create")
-<<<<<<< Updated upstream
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
-        EventData.add(newEvent);
-        return "redirect:";
-    }
-
-    @GetMapping("delete")
-    public String displayDeleteEventForm(Model model) {
-        model.addAttribute("title","Delete Events");
-        model.addAttribute("events",EventData.getAll());
-        return "events/delete";
-    }
-
-    @PostMapping("delete")
-    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
-        if (eventIds !=null) {
-            for (int id : eventIds){
-                EventData.remove(id);
-            }
-        }
-=======
     public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
         events.add(new Event(eventName,eventDescription));
->>>>>>> Stashed changes
         return "redirect:";
     }
 
-    @GetMapping("edit")
-    public String displayEditForm(Model model, @PathVariable int eventId) {
-        // controller code will go here
-        model.addAttribute("title","Edit Events");
-        model.addAttribute("Edit Event ${event.name} (id=${id})",EventData.getById(eventId));
-        return "events/edit/{eventId}";
-    }
-
-    @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
-        // controller code will go here
-
-        return "redirect:";
-    }
 }
