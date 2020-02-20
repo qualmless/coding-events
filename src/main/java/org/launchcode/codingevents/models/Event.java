078641d54165fbb9b5daac28bab2379e8431182f
qualmless <terasmash@gmail.com>
2020-02-20 13:30:20 -0600

    version control

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,51 +1,12 @@
 package org.launchcode.codingevents.models;
 
-<<<<<<< Updated upstream
-import java.util.Objects;
-
 public class Event {
 
-    private int id;
-    private static int nextId = 1;
-
-=======
-public class Event {
-
->>>>>>> Stashed changes
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
-<<<<<<< Updated upstream
-        this.description=description;
-        this.id = nextId++;
-    }
-
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
-
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
-
-    public int getId() { return id; }
-
-    @Override
-    public String toString() { return name; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() { return Objects.hash(id); }
-=======
         this.description = description;
     }
 
@@ -71,5 +32,4 @@ public class Event {
     }
 
 
->>>>>>> Stashed changes
 }
