385fd69a730f1ff0e41138c613c02baa285346c5
qualmless <terasmash@gmail.com>
2020-03-05 15:11:17 -0600

    EventCategory exercise created

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -17,19 +17,15 @@ public class EventCategory {
         this.name=name;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
