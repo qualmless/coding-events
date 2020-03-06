b807cf28bddab4a8497caf630be4bd12509be060
qualmless <terasmash@gmail.com>
2020-03-06 15:36:19 -0600

    EventCategory create form working, submitting form not working

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -37,10 +37,10 @@ public class EventCategoryController {
 
     @PostMapping("create")
     public String processCreateEventCategoryForm (@ModelAttribute @Valid EventCategory newEventCategory,
-                                                  Errors errors,
-                                                  Model model){
+                                                  Errors errors, Model model){
         if(errors.hasErrors()) {
             model.addAttribute("title", "Create Category");
+            model.addAttribute(new EventCategory());
             return "eventCategories/create";
         }
         eventCategoryRepository.save(newEventCategory);
