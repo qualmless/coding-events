556c04da587577c35ef91c1e17752ea33681674c
qualmless <terasmash@gmail.com>
2020-03-11 12:59:30 -0500

    created Tags and several bits before looking

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -1,8 +1,32 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Tag extends AbstractEntity {
 
+    @NotBlank(message = "Tag name is required")
+    @Size(max = 50, message = "Tag can be no more than 50 characters")
+    private String name;
+
+    public Tag(@NotBlank(message = "Tag name is required") @Size(max = 50, message = "Tag can be no more than 50 characters") String name) {
+        this.name = name;
+    }
+
+    public Tag(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
