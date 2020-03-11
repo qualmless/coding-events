becc83815e0f7bd7cd9a7734c0156e6b8ac263d2
qualmless <terasmash@gmail.com>
2020-03-11 14:19:49 -0500

    tag table not showing up, but database populating

--- src/main/java/org/launchcode/codingevents/controllers/TagController.java
+++ src/main/java/org/launchcode/codingevents/controllers/TagController.java
@@ -2,7 +2,6 @@ package org.launchcode.codingevents.controllers;
 
 
 import org.launchcode.codingevents.data.TagRepository;
-import org.launchcode.codingevents.models.EventCategory;
 import org.launchcode.codingevents.models.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -48,4 +47,6 @@ public class TagController {
         tagRepository.save(tag);
         return "redirect:";
     }
+
+
 }
