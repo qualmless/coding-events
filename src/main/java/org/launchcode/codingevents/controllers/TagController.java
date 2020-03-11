b06e44cab17b811d076d87864e0805dcc150a80e
qualmless <terasmash@gmail.com>
2020-03-11 13:21:33 -0500

    create tags works, all tags does not

--- src/main/java/org/launchcode/codingevents/controllers/TagController.java
+++ src/main/java/org/launchcode/codingevents/controllers/TagController.java
@@ -2,9 +2,18 @@ package org.launchcode.codingevents.controllers;
 
 
 import org.launchcode.codingevents.data.TagRepository;
+import org.launchcode.codingevents.models.EventCategory;
+import org.launchcode.codingevents.models.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("tags")
@@ -12,4 +21,31 @@ public class TagController {
 
     @Autowired
     private TagRepository tagRepository;
+
+    @GetMapping
+    public String displayAllTags(Model model) {
+        model.addAttribute("title", "All Tags");
+        model.addAttribute("categories", tagRepository.findAll());
+        return "tags/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateTagsForm(Model model) {
+        model.addAttribute("title", "Create Tags");
+        model.addAttribute(new Tag());
+        return "tags/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateTagsForm(@Valid @ModelAttribute Tag tag,
+                                                 Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Tag");
+            model.addAttribute(new Tag());
+            return "tags/create";
+        }
+        tagRepository.save(tag);
+        return "redirect:";
+    }
 }
