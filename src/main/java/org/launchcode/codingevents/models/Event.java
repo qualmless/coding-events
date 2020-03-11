55a4039b19ad6ded7ee3b93b496112b4093c2c44
qualmless <terasmash@gmail.com>
2020-03-11 10:56:43 -0500

    manytoone annotation set up on event class with EventCategory

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,8 +3,10 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
@@ -13,10 +15,6 @@ import java.util.Objects;
  */
 @Entity
 public class Event extends AbstractEntity{
-
-//    @Id
-//    @GeneratedValue
-//    private int id;
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -29,13 +27,15 @@ public class Event extends AbstractEntity{
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    private EventType type;
+    @ManyToOne
+    @NotNull(message="Category is required")
+    private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventType type) {
+    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
-        this.type = type;
+        this.eventCategory = eventCategory;
     }
 
     public Event() {}
@@ -64,33 +64,16 @@ public class Event extends AbstractEntity{
         this.contactEmail = contactEmail;
     }
 
-    public EventType getType() {
-        return type;
+    public EventCategory getEventCategory() {
+        return eventCategory;
     }
 
-    public void setType(EventType type) {
-        this.type = type;
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
     }
-
-//    public int getId() {
-//        return id;
-//    }
 
     @Override
     public String toString() {
         return name;
     }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Event event = (Event) o;
-//        return id == event.id;
-//    }
-
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
 }
--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -12,10 +12,6 @@ import java.util.Objects;
 @Entity
 public class EventCategory extends AbstractEntity{
 
-//    @Id
-//    @GeneratedValue
-//    private int id;
-
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
@@ -33,25 +29,9 @@ public class EventCategory extends AbstractEntity{
         this.name = name;
     }
 
-//    public int getId() {
-//        return id;
-//    }
-
     @Override
     public String toString() {
         return name;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        EventCategory that = (EventCategory) o;
-//        return id == that.id;
-//    }
-
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
 }
